bc27f4d1bc634433178e6e7879485ca8187b86ac
root <root@test.robointerativo.ru>
2023-05-03 20:07:47 +0000

    1

---
+++ letsenc/run.sh
@@ -0,0 +1,7 @@
+# Requires access to port 80 from the internet, adjust your firewall if needed.
+docker run --rm -it \
+  -v "${PWD}/docker-data/certbot/certs/:/etc/letsencrypt/" \
+  -v "${PWD}/docker-data/certbot/logs/:/var/log/letsencrypt/" \
+  -p 80:80 \
+  certbot/certbot certonly --standalone -d  test.robointerativo.ru
+
